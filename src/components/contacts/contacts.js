4eef4402f1122f84833fb0fa2249a21a623c9968
Olga Rozhkova <olga.gavrilova.og94@gmail.com>
2021-07-19 16:59:16 +0300

    add changes to section contacts

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -10,14 +10,15 @@ export default function Contacts() {
             <ul className='contacts_block__main'l>
                 <li>Location: Republic of Belarus. Minsk.</li>
                 <li>Phone: <a href="tel:+375292735778">+(375)29-273-57-78</a></li>
-                <li>Email: olga.gavrilova.og94@gmail.com</li>
-                <li><a href="https://rabota.by/resume/a315511aff08ea1d1b0039ed1f336f52516a53" target="blank">My CV on rabota.by</a> </li>
+                <li>Telegram: <a href="https://t.me/olya_or">t.me/olya_or</a></li>
+                <li>Email: <a href="mailto:olga.rozhkova.og94@gmail.com">olga.gavrilova.og94@gmail.com</a></li>
+                <li>My CV Rabota.by: <a href="https://rabota.by/resume/a315511aff08ea1d1b0039ed1f336f52516a53" target="blank">rabota.by/resume/..</a> </li>
             </ul>
             <ul className='contacts_block__social-network'>
-                <li>Github: <a href="https://github.com/OlyaOR" target="blank">https://github.com/OlyaOR</a></li>
-                <li><a href="https://www.linkedin.com/in/olya-rozhkova-or" target="blank">LinkedIn</a></li>
-                <li><a href="https://vk.com/olyaroa" target="blank">VKontakte</a></li>
-                <li>Instagram:<a href="https://www.instagram.com/olya.rozhkova.or" target="blank">@olya.rozhkova.or</a></li>
+                <li>Github: <a href="https://github.com/OlyaOR" target="blank">github.com/OlyaOR</a></li>
+                <li>LinkedIn: <a href="https://www.linkedin.com/in/olya-rozhkova-or" target="blank">linkedin.com/in/olya-rozhkova-or</a></li>
+                <li>VKontakte: <a href="https://vk.com/olyaroa" target="blank">vk.com/olyaroa</a></li>
+                <li>Instagram: <a href="https://www.instagram.com/olya.rozhkova.or" target="blank">@olya.rozhkova.or</a></li>
             </ul>
             
         </div>
